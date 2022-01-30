fdb45c6141c720f066f76e8bcf04cda3d840409c
Anri Lombard <anri.m.lombard@gmail.com>
2022-01-30 15:23:17 +0200

    Tidy up project structure

--- Assets/Scripts/Core/FollowPlayer.cs
+++ Assets/Scripts/Core/FollowPlayer.cs
@@ -12,7 +12,7 @@ public class FollowPlayer : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         transform.position = target.position;
     }
